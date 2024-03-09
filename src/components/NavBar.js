2f13ca93356431f59e7871b169473f0a37cf2dba
frankassafuah <frankassafuah@gmail.com>
2024-03-09 21:48:26 +0000

    project setup, split components, prop drilling

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+export default function NavBar({ movies }) {
+  const [query, setQuery] = useState("");
+
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search query={query} setQuery={setQuery} />
+      <Numresults movies={movies} />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function Search({ query, setQuery }) {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function Numresults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
