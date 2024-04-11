c6b978e27b72d41a66f129e839968239fe88755b
frankassafuah <frankassafuah@gmail.com>
2024-04-11 13:29:29 +0000

    clean up data fetching

--- src/App.js
+++ src/App.js
@@ -72,18 +72,27 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovie() {
-        setIsLoading(true);
-        const res = await fetch(
-          `https://api.themoviedb.org/3/search/movie?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1&query=${query}`
-        );
-        const data = await res.json();
-        console.log(data);
-        setMovies(data.results);
-        setIsLoading(false);
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1&query=${query}`,
+            { signal: controller.signal }
+          );
+          const data = await res.json();
+          setMovies(data.results);
+          setIsLoading(false);
+        } catch (error) {
+          console.error(error.message);
+        }
       }
       if (query.length < 3) return;
       fetchMovie();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
